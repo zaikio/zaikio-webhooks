e814c5bffe8743159b6ec3985211417649cee1ca
Jalyna <jalyna.schroeder@gmail.com>
2020-03-26 16:06:07 +0100

    Add missing test case

--- test/controllers/zaikio/webhook/webhooks_controller_test.rb
+++ test/controllers/zaikio/webhook/webhooks_controller_test.rb
@@ -5,6 +5,14 @@ module Zaikio
     class WebhooksControllerTest < ActionDispatch::IntegrationTest
       include Engine.routes.url_helpers
 
+      def setup
+        Zaikio::Webhook.configure do |config|
+          config.register_client :my_app do |my_app|
+            my_app.shared_secret = "test-secret"
+          end
+        end
+      end
+
       def signature(shared_secret, data)
         OpenSSL::HMAC.hexdigest("SHA256", shared_secret, data.to_json)
       end
@@ -24,8 +32,20 @@ module Zaikio
           }
         }
         WebhookExecutionJob.expects(:perform_later).with(data)
+        WebhookExecutionJob.expects(:perform_now).never
         post zaikio_webhook.root_path("my_app"), params: data.to_json, headers: {
           "X-Loom-Signature" => signature("test-secret", data),
+          "Content-Type" => "application/json"
+        }
+        assert_response :success
+      end
+
+      test "does nothing with wrong signature" do
+        data = { "name" => "directory.revoked_access_token" }
+        WebhookExecutionJob.expects(:perform_later).never
+        WebhookExecutionJob.expects(:perform_now).never
+        post zaikio_webhook.root_path("my_app"), params: data.to_json, headers: {
+          "X-Loom-Signature" => signature("wrong-secret", data),
           "Content-Type" => "application/json"
         }
         assert_response :success
